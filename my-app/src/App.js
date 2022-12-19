e4c357f15524afdf4edfa0132abe5e4ed30e533a
harry <harwindersinghmalhi1@gmail.com>
2022-12-19 04:14:05 -0500

    darkmode about

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,18 @@
 
 import './App.css';
+import About from './Componet/About';
 import Navbar from './Componet/Navbar';
-import Textarea from './Componet/Textarea';
+// import Textarea from './Componet/Textarea';
 
 function App() {
   return (
     <>
-    <Navbar title="Home" about="info" link="referance"/>
-    <div className="container">
-     <Textarea Textarea="Textarea"  rows="9"/></div>
+    <Navbar title="Home" about="info" link="about us"/>
+    <div className="container my-3 7px solid red">
+     {/* <Textarea Textarea="Textarea" /> */}
+     <About />
+     
+     </div>
      
      
      </>
--- my-app/src/Componet/Textarea.js
+++ my-app/src/Componet/Textarea.js
@@ -1,24 +1,37 @@
 import React, { useState } from 'react'
 function Textarea(props) {
 
-    const[text,setText] = useState("enter the text")
+   const[Text,setText] = useState("enter the text above box")
       const changed=(event)=>{
-        setText(event.target.value)
-      }
+        setText(event.target.value)}
       const clicked=()=>{
-        let newtext = text.toLocaleUpperCase();
-           setText(newtext);
-      }
+        let newtext = Text.toLocaleUpperCase();
+           setText(newtext);}
+      
+      const tolower=()=>{let lowertext = Text.toLocaleLowerCase()
+           setText(lowertext); }
+
     return (
+    <>
     <div>
      <h1>Bootsrap{props.Textarea}</h1>
       <div className="form-floating my-3">
-  <textarea className="form-control" rows="9" id="mybox" onChange={changed} value={text}></textarea>
-  <button className='btn btn-primary' onClick={clicked}>Uppercase</button>
+  <textarea className="form-control" rows="9" id="mybox" onChange={changed} placeholder="Enter text"></textarea>
+  <button className='btn btn-primary mx-1 my-2' onClick={clicked}>Uppercase</button>
+  <button className='btn btn-primary mx-1 my-2' onClick={tolower}>lowercase</button>
 
-</div>
-    </div>
+     </div>
+     </div>
+    <div className="container">
+   <h2>this is summary</h2>
+   <p>{Text.split("").length} words and {Text.length} chracters </p>
+   <p>{0.08*Text.split("").length} seconds , one average person take read above text. </p>
+   <h2>preview</h2>
+   <p>{Text}</p>
+      </div>
+
+    </>
   )
 }
 
-export default Textarea
+ export default Textarea ;
